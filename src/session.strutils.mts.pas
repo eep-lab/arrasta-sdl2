225eaa9549f10a97c57a0fe7d0a49a6f2011f71c
Rafael Picanço <cpicanco@users.noreply.github.com>
2023-10-29 20:05:03 -0300

    refact: inform that stringlist will not change

--- src/session.strutils.mts.pas
+++ src/session.strutils.mts.pas
@@ -7,14 +7,16 @@ interface
 uses
   Classes, SysUtils;
 
-function GetWordValue(AParameters: TStringList; AIsSample : boolean;
+
+
+function GetWordValue(const AParameters: TStringList; AIsSample : boolean;
   AIndex : integer = -1) : string;
 
 implementation
 
 uses session.constants.mts;
 
-function GetWordValue(AParameters: TStringList; AIsSample: boolean;
+function GetWordValue(const AParameters: TStringList; AIsSample: boolean;
   AIndex: integer): string;
 begin
   if AIsSample then begin
